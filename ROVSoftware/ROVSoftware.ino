11231c36d272042c91a74ff29bf16f58bb07a513
jayadamsmorgan <applejay1802@gmail.com>
2017-02-01 00:35:59 +1000

    minor changes
    added some variables, udp connection, manipulator rotate & tight functions, changed motor controlling to support minSpeed & maxSpeed

--- ROVSoftware/ROVSoftware.ino
+++ ROVSoftware/ROVSoftware.ino
@@ -4,32 +4,52 @@
 #include <Servo.h>
 #include <PID_v1.h>
 
-#define MOTOR1PIN 7 //some pin
-#define MOTOR2PIN 8 //some pin
-#define MOTOR3PIN 9 //some pin
-#define MOTOR4PIN 10 //some pin
-#define MOTOR5PIN 11 //some pin
-#define MOTOR6PIN 12 //some pin
+#define MOTOR1PIN 7 // Some pin
+#define MOTOR2PIN 8 // Some pin
+#define MOTOR3PIN 9 // Some pin
+#define MOTOR4PIN 10 // Some pin
+#define MOTOR5PIN 11 // Some pin
+#define MOTOR6PIN 12 // Some pin
 
-#define SERVO_MANIPULATOR_PIN 13 //some pin
-#define SERVO_CAMERA_PIN 14 //some pin
+#define SERVO_MANIPULATOR_PIN 13 // Some pin
+#define SERVO_CAMERA_PIN 14 // Some pin
 
 #define SERVO_UPDATE_WINDOW 30 // Delay for updating servo's angle
 #define DEPTH_AND_PITCH_UPDATE_WINDOW 250 // Delay for switching autoDepth & autoPitch
 
+#define CAMERA_ANGLE_DELTA 3
+#define MIN_CAMERA_ANGLE 0 // ?
+#define MAX_CAMERA_ANGLE 160 // ?
+#define MAX_BOTTOM_MANIP_ANGLE 160
+#define MIN_BOTTOM_MANIP_ANGLE 100
+
+#define INCOMING_PACKET_SIZE 9
+#define OUTCOMING_PACKET_SIZE 10
+
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,111);
+IPAddress ip(192, 168, 1, 242), remote_device;
+char packetBuffer[INCOMING_PACKET_SIZE]; // Buffer to hold incoming packet,
+unsigned char replyBuffer[OUTCOMING_PACKET_SIZE]; // A string to send back
+EthernetUDP Udp;
   
 Servo horMotor1, horMotor2, horMotor3, horMotor4;
 Servo verMotor1, verMotor2;
 
-float yaw = 0, pitch = 0, roll = 0;
+Servo camera, bottomManip;
+int camera_angle = (MAX_CAMERA_ANGLE + MIN_CAMERA_ANGLE) / 2, bottom_manip_angle = MAX_BOTTOM_MANIP_ANGLE;
+unsigned long long prev_camera_servo_update, prev_manip_servo_update;
 
-short min_speed = 0, max_speed = 255;
+float yaw = 0, pitch = 0, roll = 0;
+  
+short min_speed = 0, max_speed = 400;
+
 signed char js_val[5];
+bool buttons[7];
+
 bool isAutoDepth = false, isAutoPitch = false, isAutoYaw = false;
 
 unsigned char depth_and_pitch_update = 0;
+unsigned long long prev_camera_servo_update, prev_manip_servo_update;
 
 // Function for controlling motor system
 void controlPeripherals() {
@@ -105,6 +125,58 @@ char depthPID() {
 char yawPID() {
   // TODO calculation of yawPID
 }
+
+// Receiving messages from PC & parsing
+char receiveMessage() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize > 0){
+    Serial.print("Received packet of size: ");
+    Serial.println(packetSize);
+  }
+  if(packetSize == INCOMING_PACKET_SIZE) {
+    Serial.println("Received packet with correct size");
+    remote_device = Udp.remoteIP();
+    Udp.read(packetBuffer , INCOMING_PACKET_SIZE);
+    for (int i = 0; i < 5; ++i)
+      js_val[i] = (signed char)packetBuffer[i];
+    min_speed = (unsigned char)packetBuffer[5];
+    max_speed = (unsigned char)packetBuffer[6];
+    isAutoDepth = packetBuffer[8] & 1;
+    isAutoPitch = (packetBuffer[8] >> 1) & 1;
+    isAutoYaw = (packetBuffer[8] >> 2) & 1;
+    for (int i = 0; i < 7; ++i){
+      buttons[i] = (packetBuffer[7] >> i) & 1;
+    }
+    return 1;
+  }
+  else
+    return 0;
+}
+
+// Forming & sending packet to PC via UDP
+void sendReply() {
+  Serial.print("PC is on :"); Serial.println(remote_device);
+  replyBuffer[0] = (depth >> 8) & 0xFF;
+  replyBuffer[1] = depth & 0xFF;
+  yaw *= 100;
+  pitch *= 100;
+  roll *= 100;
+  replyBuffer[2] = ((int)yaw >> 8) & 0xFF;
+  replyBuffer[3] = ((int)yaw) & 0xFF;
+  replyBuffer[4] = ((int)pitch >> 8) & 0xFF;
+  replyBuffer[5] = ((int)pitch) & 0xFF;
+  replyBuffer[6] = ((int)roll >> 8) & 0xFF;
+  replyBuffer[7] = ((int)roll) & 0xFF;
+  
+  Serial.println("Replying...");
+  Udp.beginPacket(remote_device, Udp.remotePort());
+  Serial.println("Forming packet");
+  Serial.println(Udp.write(replyBuffer, OUTCOMING_PACKET_SIZE));
+  Serial.println("Writing packet");
+  Udp.endPacket();
+  Serial.println("Endpacket");
+  return;
+}
     
 // Function to control horizontal brushless motors
 void horizontalMotorControl(Servo motor, char x, char y, char z) {
@@ -113,8 +185,8 @@ void horizontalMotorControl(Servo motor, char x, char y, char z) {
   if(sum > 100) sum = 100;
   if(sum < (-100)) sum = -100;
   Serial.print("Horizontal motor pow: "); Serial.println(POW);
-  POW = map(POW, -100, 100, 1060, 1860);
-  motor.writeMicroseconds(POW);
+  POW = short(sum * (float(max_speed - min_speed) / 100));
+  motor.writeMicroseconds(1460 + POW);
 }
 
 // Function to control vertical brushless motors
@@ -124,10 +196,41 @@ void verticalMotorControl(Servo motor, short z) {
   if(sum > 100) sum = 100;
   if(sum < (-100)) sum = -100;
   Serial.print("Vertical motor pow: "); Serial.println(POW);
-  POW = map(POW, -100, 100, 1060, 1860);
-  motor.writeMicroseconds(POW);
+  POW = short(sum * (float(max_speed - min_speed) / 100));
+  motor.writeMicroseconds(1460 + POW);
 }
 
+// Function to rotate manipulator
+void rotateManipulator(char PinA, char PinB, char dir) {
+  if (dir > 0){
+    digitalWrite(PinA, HIGH);
+    digitalWrite(PinB, LOW);
+  }
+  if (dir < 0){
+    digitalWrite(PinA, LOW);
+    digitalWrite(PinB, HIGH);
+  }
+  if (dir == 0){
+    digitalWrite(PinA, LOW);
+    digitalWrite(PinB, LOW);
+  }
+}
+
+// Function to tight manipulator
+void tightenManipulator(char PinA, char PinB, char dir) {
+  if (dir > 0){
+    digitalWrite(PinA, HIGH);
+    digitalWrite(PinB, LOW);
+  }
+  if (dir < 0){
+    digitalWrite(PinA, LOW);
+    digitalWrite(PinB, HIGH);
+  }
+  if (dir == 0){
+    digitalWrite(PinA, LOW);
+    digitalWrite(PinB, LOW);
+  }
+}
 
 void setup() {
   // Init I2C connection for IMU
@@ -146,6 +249,15 @@ void setup() {
   horMotor4.write(90);
   verMotor1.write(90);
   verMotor2.write(90);
+  
+  Ethernet.begin(mac,ip);
+  Udp.begin(8000);
+  Serial.begin(115200);
+  
+  camera.attach(SERVO_CAMERA_PIN);
+  camera.write(camera_angle);
+  bottom_manip.attach(SERVO_MANIPULATOR_PIN);
+  bottom_manip.write(bottom_manip_angle);
   
   // Some delay for motors...
   delay(2000);
