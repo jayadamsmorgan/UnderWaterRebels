3b61b1bfbc201adefdbad8d17c5233b39bfea622
jayadamsmorgan <applejay1802@gmail.com>
2017-04-24 21:30:49 +1000

    a little update

--- ROVJoystick/core/src/com/germanberdnikov/rov/Main.java
+++ ROVJoystick/core/src/com/germanberdnikov/rov/Main.java
@@ -3,42 +3,41 @@ package com.germanberdnikov.rov;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    private SpriteBatch batch;
+    private UDPThread udpThread;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        udpThread = new UDPThread();
+        udpThread.start();
+    }
 
-	@Override
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
+        //udpThread.setData(-100, 100, 44, 33, 22, 11, 0, 0, 0, true, true, false);
+    }
+
+    @Override
     public void resume() {
-
+        udpThread.setRunning(true);
     }
 
-	@Override
+    @Override
     public void pause() {
-
+        udpThread.setRunning(false);
     }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
 
--- ROVJoystick/core/src/com/germanberdnikov/rov/UDPThread.java
+++ ROVJoystick/core/src/com/germanberdnikov/rov/UDPThread.java
@@ -1,42 +1,93 @@
 package com.germanberdnikov.rov;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.BitSet;
 
 class UDPThread extends Thread {
 
-    boolean isRunning;
-    DatagramSocket socket;
-    int serverPort = 8001;
-    byte data[];
+    private boolean isRunning;
+    private DatagramSocket socket;
+    private byte data[];
+    private int ip1 = 192, ip2 = 168, ip3 = 1, ip4 = 124;
 
     UDPThread() {
         isRunning = true;
-
+        data = new byte[7];
     }
 
-    public void setRunning(boolean isRunning) {
+    void setRunning(boolean isRunning) {
         this.isRunning = isRunning;
     }
 
-    public void setData(char xAxis, char yAxis, char zAxis, char rAxis, char wAxis,
-                        char rotCam, char manTight, char botMan, char manRot,
-                        char speedMode,
-                        boolean isAutoYaw, boolean isAutoPitch, boolean isAutoDepth) {
-        
+    void setData(int xAxis, int yAxis, int zAxis, int rAxis, int wAxis,
+                 int rotCam, int manTight, int botMan, int speedMode,
+                 boolean isAutoYaw, boolean isAutoPitch, boolean isAutoDepth) {
+        // Values are from -100 to 100, so we can use 1 byte to send every axis
+        data[0] = (byte) xAxis;
+        data[1] = (byte) yAxis;
+        data[2] = (byte) zAxis;
+        data[3] = (byte) rAxis;
+        data[4] = (byte) wAxis;
+
+        // UNTESTED YET
+        BitSet bitSet = new BitSet(1);
+        if (rotCam < 0) {
+            bitSet.set(1, true);
+            data[5] = bitToByte(bitSet, 7);
+            bitSet.set(1, false);
+            data[5] = bitToByte(bitSet, 6);
+        } else if (rotCam > 0) {
+
+        } else {
+
+        }
+        if (manTight < 0) {
+
+        } else if (manTight > 0) {
+
+        } else {
+
+        }
+        if (botMan < 0) {
+
+        } else if (botMan > 0) {
+
+        } else {
+
+        }
+        if (speedMode == 1) {
+
+        } else if (speedMode == 2) {
+
+        } else if (speedMode == 3) {
+
+        }
+
     }
 
-    private void getBytes() {
-
+    private static byte bitToByte(BitSet bits, int offset) {
+        byte value = 0;
+        for (int i = offset; (i < bits.length() && ((i + offset) < 8)); ++i) {
+            value += bits.get(i) ? (1 << i) : 0;
+        }
+        return value;
     }
 
     @Override
     public void run() {
         try {
+            int serverPort = 8001;
             socket = new DatagramSocket(serverPort);
-            while (isRunning) {
-                getBytes();
+            InetAddress address = InetAddress.getByAddress(new byte[]{(byte) ip1, (byte) ip2, (byte) ip3, (byte) ip4});
+            while (true) {
+                while (isRunning) {
+                    DatagramPacket packet = new DatagramPacket(data, data.length, address, serverPort);
+                    socket.send(packet);
+                }
             }
         } catch (SocketException e) {
             e.printStackTrace();
