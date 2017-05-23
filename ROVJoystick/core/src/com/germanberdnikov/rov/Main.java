59b47b84614bc6753ddfc92eec663c061fe647f1
jayadamsmorgan <applejay1802@gmail.com>
2017-05-23 13:44:44 +1000

    update

--- ROVJoystick/core/src/com/germanberdnikov/rov/Main.java
+++ ROVJoystick/core/src/com/germanberdnikov/rov/Main.java
@@ -6,80 +6,93 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Stage mainStage;
-
     // UDPThread for udp connection with PC
     private UDPThread udpThread;
 
     // UI objects:
-    private Slider zSlider, rSlider;
+    private SpriteBatch batch;
+    private Stage mainStage;
+    private Slider zSlider, rSlider, rMainManipSlider, camSlider, tBotManipSlider, speedSlider;
     private CustomTouchPad xyTouchPad;
+    private Button tightMainManipButton, untightMainManipButton,
+            autoPitchButton, autoYawButton, autoDepthButton,
+            multiplexorButton, ledButton;
 
-    // JS Data
-    private int xAxis, yAxis, zAxis, rzAxis, wAxis;
+    // JS Data:
+    private int xAxis, yAxis, zAxis, rzAxis;
     private boolean isAutoYaw, isAutoPitch, isAutoDepth, isLED;
     private int speedMode, muxChannel;
-    private int cameraRotateDirection, manipulatorTightDirection, manipulatorRotateDirection,
-            bottomManipulatorDirection;
+    private int cameraRotateDirection, mainManipTightDirection, mainManipRotateDirection,
+            bottomManipDirection;
+
+    // Paths:
+    private String zSliderBackground = "ui/sliders/zSlider/zSliderBackground.png";
+    private String zSliderKnob = "ui/sliders/zSlider/zSliderKnob.png";
+    private String speedSliderBackground = "ui/sliders/speedSlider/speedSliderBackground.png";
+    private String speedSliderKnob = "ui/sliders/speedSlider/speedSliderKnob.png";
+    private String rSliderBackground = "ui/sliders/rSlider/rSliderBackground.png";
+    private String rSliderKnob = "ui/sliders/rSlider/rSliderKnob.png";
+    private String xyTouchPadBackground = "ui/touchPads/xyTouchPad/xyTouchPadBackground.png";
+    private String xyTouchPadKnob = "ui/touchPads/xyTouchPad/xyTouchPadKnob.png";
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         mainStage = new Stage();
 
-        Image zSliderBackgroundImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/sliders/zSlider/zSliderBackground.png")));
-        Image zSliderKnobImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/sliders/zSlider/zSliderKnob.png")));
-        Drawable zSliderBackground = zSliderBackgroundImage.getDrawable();
-        Drawable zSliderKnob = zSliderKnobImage.getDrawable();
-        Slider.SliderStyle zSliderStyle = new Slider.SliderStyle(zSliderBackground,
-                zSliderKnob);
-        zSlider = new Slider(-100f, 100f, 1f, true, zSliderStyle);
+        zSlider = createNewSlider(zSliderBackground, zSliderKnob,
+                -100f, 100f, 1f, true);
 
-        Image rSliderBackgroundImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/sliders/rSlider/rSliderBackground.png")));
-        Image rSliderKnobImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/sliders/rSlider/rSliderKnob.png")));
-        Drawable rSliderBackground = rSliderBackgroundImage.getDrawable();
-        Drawable rSliderKnob = rSliderKnobImage.getDrawable();
-        Slider.SliderStyle rSliderStyle = new Slider.SliderStyle(rSliderBackground, rSliderKnob);
-        rSlider = new Slider(-100f, 100f, 1f, false, rSliderStyle);
+        speedSlider = createNewSlider(speedSliderBackground, speedSliderKnob,
+                0f, 3f, 1f,false);
 
-        Image xyTouchPadBackgroundImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/touchPads/xyTouchPad/xyTouchPadBackground.png")));
-        Image xyTouchPadKnobImage = new Image(
-                new Texture(Gdx.files.internal(
-                        "ui/touchPads/xyTouchPad/xyTouchPadKnob.png")));
-        Drawable xyTouchPadBackground = xyTouchPadBackgroundImage.getDrawable();
-        Drawable xyTouchPadKnob = xyTouchPadKnobImage.getDrawable();
-        Touchpad.TouchpadStyle xyTouchPadStyle = new Touchpad.TouchpadStyle(xyTouchPadBackground,
-                xyTouchPadKnob);
-        xyTouchPad = new CustomTouchPad(5f, xyTouchPadStyle);
+        rSlider = createNewSlider(rSliderBackground, rSliderKnob,
+                -100f, 100f, 1f,false);
+
+        xyTouchPad = createNewTouchPad(xyTouchPadBackground, xyTouchPadKnob, 5f);
 
         mainStage.addActor(zSlider);
         mainStage.addActor(rSlider);
+        mainStage.addActor(speedSlider);
         mainStage.addActor(xyTouchPad);
 
-        setActorsPositions();
         setActorsSize();
+        setActorsPositions();
 
         udpThread = new UDPThread();
         udpThread.start();
 
         Gdx.input.setInputProcessor(mainStage);
+    }
+
+    private Slider createNewSlider(String backgroundPath, String knobPath,
+                                   float minValue, float maxValue, float stepSize,
+                                   boolean isVertical) {
+        Image backgroundImage = new Image(new Texture(Gdx.files.internal(backgroundPath)));
+        Image knobImage = new Image(new Texture(Gdx.files.internal(knobPath)));
+        Drawable sliderBackground = backgroundImage.getDrawable();
+        Drawable sliderKnob = knobImage.getDrawable();
+        Slider.SliderStyle sliderStyle = new Slider.SliderStyle(sliderBackground, sliderKnob);
+        return new Slider(minValue, maxValue, stepSize, isVertical, sliderStyle);
+    }
+
+    private CustomTouchPad createNewTouchPad(String backgroundPath, String knobPath,
+                                             float deadzoneRadius) {
+        Image touchPadBackgroundImage = new Image(
+                new Texture(Gdx.files.internal(backgroundPath)));
+        Image touchPadKnobImage = new Image(new Texture(Gdx.files.internal(knobPath)));
+        Drawable touchPadBackground = touchPadBackgroundImage.getDrawable();
+        Drawable touchPadKnob = touchPadKnobImage.getDrawable();
+        Touchpad.TouchpadStyle touchPadStyle = new Touchpad.TouchpadStyle(touchPadBackground,
+                touchPadKnob);
+        return new CustomTouchPad(deadzoneRadius, touchPadStyle);
     }
 
     @Override
@@ -95,8 +108,8 @@ class Main extends ApplicationAdapter {
     }
 
     private void preparePacket() {
-        udpThread.setData(xAxis, yAxis, zAxis, rzAxis, wAxis,
-                cameraRotateDirection, manipulatorTightDirection, bottomManipulatorDirection,
+        udpThread.setData(xAxis, yAxis, zAxis, rzAxis, mainManipRotateDirection,
+                cameraRotateDirection, mainManipTightDirection, bottomManipDirection,
                 speedMode, muxChannel, isAutoYaw, isAutoPitch, isAutoDepth, isLED);
     }
 
